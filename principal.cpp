2451969e14e2454179e6e2ca546aba0d8d136cb3
Daniel9ad <daniel.ad.dad9@gmail.com>
2022-04-05 09:28:50 -0400

    Modificacion 1

--- principal.cpp
+++ principal.cpp
@@ -3,10 +3,23 @@
 using namespace std;
 
 int main() {
-    cout << "Daniel Avendaño Duran" << endl;
-    cout << "Biografia" << endl;
-    cout << "Naci en Sucre, Chuquisaca" << endl;
-    cout << "Estudie en los colegios: Ovando, Bernardo Monteagudo, Mariano y Lucio Siles" << endl;
-    cout << "Sali bachiller en el colegio Lucio Siles" << endl;
-    cout << "Estudio Ing. en ciencias de la computacion" << endl;
+
+    int n;
+
+    do {
+        cout << "Daniel Avendaño Duran" << endl;
+        cout << "Biografia" << endl;
+        cout << "Naci en Sucre, Chuquisaca" << endl;
+        cout << "Estudie en los colegios: Ovando, Bernardo Monteagudo, Mariano y Lucio Siles" << endl;
+        cout << "Sali bachiller en el colegio Lucio Siles" << endl;
+        cout << "Estudio Ing. en ciencias de la computacion" << endl;
+
+        cout << "1.- Imprimir de nuevo" << endl;
+        cout << "2.- Salir" << endl;
+        
+        cin >> n;
+        
+
+    } while (n==1);
+
 }
