e2faf53a060c38bd802639bbc4fd5bdc9b3fcbf2
DuranPartners <DuranPartners@LAPTOP-8C57VVMP>
2022-04-04 22:12:30 -0400

    Agregar archivos de proyecto.

---
+++ principal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Daniel Avendaño Duran" << endl;
+    cout << "Biografia" << endl;
+    cout << "Naci en Sucre, Chuquisaca" << endl;
+    cout << "Estudie en los colegios: Ovando, Bernardo Monteagudo, Mariano y Lucio Siles" << endl;
+    cout << "Sali bachiller en el colegio Lucio Siles" << endl;
+    cout << "Estudio Ing. en ciencias de la computacion" << endl;
+}
